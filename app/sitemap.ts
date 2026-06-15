cb045d790cff744f8289f79ff92b84e54b2cf476
developer1 <nextweb1js@gmail.com>
2026-06-15 13:56:47 +0500

    fix sitemap generation

---
+++ app/sitemap.ts
@@ -0,0 +1,16 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://filevia.org/",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://filevia.org/pdf-to-word",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://filevia.org/jpg-to-pdf",
+      lastModified: new Date(),
+    },
+  ];
+}
